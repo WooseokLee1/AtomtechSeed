709f962f35482479b9720d27ab34a35f5918a304
KIM <kim@KIMui-MacBookAir.local>
2025-06-18 10:32:10 +0900

    feat: Add routes for creating and viewing businesses

--- app.js
+++ app.js
@@ -23,9 +23,8 @@ const app = express();
 
 app.set("view engine", "ejs");
 
-//default directory
-const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
-const __dirname = path.dirname(__filename); // get the name of the directory
+const __filename = fileURLToPath(import.meta.url); 
+const __dirname = path.dirname(__filename); 
 app.set("views", path.join(__dirname, "views"));
 
 app.use(express.urlencoded({ extended: true }));
@@ -42,7 +41,20 @@ app.get('/business', async(req, res) => {
     console.log(business);
     res.render('businesses/index', { business });
 })
+app.get('/business/new', (req, res) => {
+    res.render('businesses/new');
+});
 
+app.post('/business', async (req, res) => {
+    const newBusiness = new Business(req.body.business); 
+    await newBusiness.save();
+    res.redirect(`/business/${newBusiness._id}`);
+});
+
+app.get('/business/:id', async (req, res) => {
+    const business = await Business.findById(req.params.id);
+    res.render('businesses/show', { business });
+});
 
 app.listen(3000, () => {
     console.log('Serving on port 3000')
